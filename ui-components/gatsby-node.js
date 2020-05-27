86a97694a4ca1b540647c0969d02b6df3cd607f8
Feisal Mohamud Ahmed <6140236@RTS-365.dk>
2020-05-27 09:24:09 +0200

    conneting gastby

---
+++ ui-components/gatsby-node.js
@@ -0,0 +1,22 @@
+const Promise = require('bluebird')
+const path = require('path')
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+
+    resolve(
+      graphql(
+        `
+        
+          `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+
+      })
+    )
+  })
+}
